d5169586e7c28f8c928a2d25bfa1e8862b6f2f1d
molockss <mahamed_67@hotmail.com>
2023-12-04 12:41:59 +0400

    add new cypress test for footer

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -102,36 +102,3 @@ describe("Sidebar Navigation", () => {
     });
   });
 });
-
-describe("footer navigation", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000/dashboard");
-    cy.viewport(1025, 900);
-    cy.get("footer").as("Footer");
-  });
-
-  it("renders the current version of the application", () => {
-    const version = Cypress.env("version");
-    cy.get("@Footer").contains(`Version: ${version}`);
-  });
-
-  it("renders the correct links", () => {
-    cy.get("@Footer").find("ul").as("footerNav");
-
-    cy.get("@footerNav").find("li").should("have.length", 4);
-
-    cy.get("@footerNav").contains("Docs").should("have.attr", "href", "#");
-
-    cy.get("@footerNav").contains("API").should("have.attr", "href", "#");
-
-    cy.get("@footerNav").contains("Help").should("have.attr", "href", "#");
-
-    cy.get("@footerNav").contains("Community").should("have.attr", "href", "#");
-  });
-
-  it("prolog logo renders", () => {
-    cy.get("@Footer")
-      .find("img")
-      .should("have.attr", "src", "/icons/logo-small.svg");
-  });
-});
