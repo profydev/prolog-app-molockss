ff18c3900c8e64f20ed790dbc2d60f8235452fe1
Mahamed Osman <102543287+molockss@users.noreply.github.com>
2023-12-04 14:55:32 +0400

    Projects loading screen (#12)
    * feat - loading icon created
    
    * loading cypress test

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -1,13 +1,62 @@
+import React, { useState, useEffect } from "react";
 import { ProjectCard } from "../project-card";
 import { useGetProjects } from "../../api/use-get-projects";
 import styles from "./project-list.module.scss";
 
 export function ProjectList() {
   const { data, isLoading, isError, error } = useGetProjects();
+  const [showLoading, setShowLoading] = useState(true);
 
-  if (isLoading) {
-    return <div>Loading</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      // Simulate a 5-second delay
+      await new Promise((resolve) => setTimeout(resolve, 1200));
+
+      // Set showLoading to false after the delay
+      setShowLoading(false);
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  if (showLoading) {
+    return (
+      <div
+        className={`${styles.loading} ${isLoading ? styles.rotate : ""}`}
+        data-testid="loading-screen"
+      >
+        <svg
+          className={`${styles.loading} ${isLoading ? styles.rotate : ""}`}
+          xmlns="http://www.w3.org/2000/svg"
+          width="64"
+          height="64"
+          viewBox="0 0 66 66"
+          fill="none"
+        >
+          <circle
+            className={styles.loading}
+            cx="33"
+            cy="33"
+            r="30"
+            fill="none"
+            strokeWidth="8"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            stroke="#7F56D9"
+            strokeDasharray="187"
+            strokeDashoffset="187"
+            style={{
+              strokeDashoffset: isLoading ? "0" : "187",
+              transition: "stroke-dashoffset 1s ease-out",
+            }}
+          />
+        </svg>
+      </div>
+    );
   }
+
+  // Actual loaded element or condition
+  const loadedElement = <div data-testid="loaded-element"></div>;
 
   if (isError) {
     console.error(error);
@@ -15,12 +64,15 @@ export function ProjectList() {
   }
 
   return (
-    <ul className={styles.list} data-testid="project-list">
-      {data?.map((project) => (
-        <li key={project.id}>
-          <ProjectCard project={project} />
-        </li>
-      ))}
-    </ul>
+    <div>
+      {loadedElement}
+      <ul className={styles.list} data-testid="project-list">
+        {data?.map((project) => (
+          <li key={project.id}>
+            <ProjectCard project={project} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
