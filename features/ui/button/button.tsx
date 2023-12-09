b4474cc173d79a58130d6b2abe62c831085e7fe1
molockss <mahamed_67@hotmail.com>
2023-12-09 20:10:17 +0400

    button component and props with default sizes

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,9 +1,38 @@
-import { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export enum ButtonSize {
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+  xlg = "xlg",
+}
+
+export enum ButtonColor {
+  primary = "primary",
+  secondary = "secondary",
+  gray = "gray",
+  empty = "empty",
+  emptygray = "emptygray",
+  error = "error",
+  emptyerror = "emptyerror",
+}
+
+type ButtonProps = {
+  children: React.ReactNode;
+  size?: ButtonSize;
+  color?: ButtonColor;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button({
+  children,
+  size = ButtonSize.md,
+  color = ButtonColor.primary,
+}: ButtonProps) {
   return (
-    <button {...props} className={classNames(styles.button, props.className)} />
+    <div className={classNames(styles.container, styles[size], styles[color])}>
+      {children}
+    </div>
   );
 }
